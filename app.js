b06fb63ed1071a2457e65d0ded7dfa6e5d1ae087
parzychu <parzych94@gmail.com>
2016-01-10 00:19:27 +0100

    (backend): Corrected app.js to pass jslint verification.
    Signed-off-by: parzychu <parzych94@gmail.com>

--- app.js
+++ app.js
@@ -1,68 +1,81 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/*global require, module*/
+(function appWrapper() {
+    'use strict';
 
-var routes = require('./routes/index');
-var tasks = require('./routes/tasks');
+    var express = require('express'),
+        path = require('path'),
+        favicon = require('serve-favicon'),
+        logger = require('morgan'),
+        cookieParser = require('cookie-parser'),
+        bodyParser = require('body-parser'),
 
-var app = express();
-var AppConfig = require('./config/appConfig');
+        app = express(),
+        server = null,
+        socket = null,
+        host = null,
+        port = null,
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+        routes = require('./routes/index'),
+        tasks = require('./routes/tasks'),
+        AppConfig = require('./config/appConfig');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/tasks', tasks);
-app.use('/', routes);
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-// error handlers
+    app.use('/api/tasks', tasks);
+    app.use('/', routes);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    // catch 404 and forward to error handler
+    app.use( function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
     });
-  });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+    // error handlers
 
-var server = app.listen(AppConfig.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
 
+    server = app.listen(AppConfig.PORT, function () {
+        host = server.address().address;
+        port = server.address().port;
 
-module.exports = app;
+        io = require('socket.io')(server);
+
+        console.log('Example app listening at http://%s:%s', host, port);
+    });
+
+    module.exports = app;
+})();
